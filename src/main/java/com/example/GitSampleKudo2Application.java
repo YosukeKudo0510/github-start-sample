c693d161e2e40c5fb9b2147f83eb17a59a8d442e
YosukeKudo0510 <yosuke.kudo@rakus-partners.co.jp>
2022-05-10 17:00:01 +0900

    firstcommit

---
+++ src/main/java/com/example/GitSampleKudo2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleKudo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleKudo2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/domain/Car.java
@@ -0,0 +1,22 @@
+package com.example.domain;
+
+public class Car {
+	
+	private String name;
+	private Integer speed;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getSpeed() {
+		return speed;
+	}
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+	
+
+}
